c3c797d52ecb46e9fc30b8ebe4fc8016b8c5f6bd
sudonetizen <sardortolibjonov1039@gmail.com>
2026-02-15 01:45:34 +0500

    add createview, updateview, deleteview

--- blog/tests.py
+++ blog/tests.py
@@ -46,3 +46,39 @@ class BlogTests(TestCase):
 
         bad_response = self.client.get("/post/999/")
         self.assertEqual(bad_response.status_code, 404)
+
+    def test_post_createview(self):
+        response = self.client.post(
+            reverse("post_new"),
+            {
+                "title": "New title",
+                "body": "New text",
+                "author": self.user.id,
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "New title")
+        self.assertEqual(Post.objects.last().body, "New text")
+
+        response = self.client.get("/post/2/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_updateview(self):
+        response = self.client.post(
+            reverse("post_edit", args="1"),
+            {
+                "title": "Updated title",
+                "body": "Updated text",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "Updated title")
+        self.assertEqual(Post.objects.last().body, "Updated text")
+
+    def test_post_deleteview(self):
+        response = self.client.post(reverse("post_delete", args="1"))
+        self.assertEqual(response.status_code, 302)
+
+        response = self.client.get("/post/1/")
+        self.assertEqual(response.status_code, 404)
+        
